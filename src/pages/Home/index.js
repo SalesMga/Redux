ddd8c488558e29b51ffefa37fb3ee69acfc650f3
SalesMga <lucassalessa3@gmail.com>
2020-04-29 03:12:08 -0300

    Estilizacao da Home (Lista, com imagem)

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,96 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <h1>Home page</h1>
+  return (
+    <ProductList>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-adidas-duramo-lite-20-masculino/02/NQQ-0375-002/NQQ-0375-002_zoom2.jpg?ts=1577985531&ims=326x"
+          alt="Tenis" />
+        <strong>Tenis legal</strong>
+        <span>R$ 129,90</span>
+
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />3
+            </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-adidas-duramo-lite-20-masculino/02/NQQ-0375-002/NQQ-0375-002_zoom2.jpg?ts=1577985531&ims=326x"
+          alt="Tenis" />
+        <strong>Tenis legal</strong>
+        <span>R$ 129,90</span>
+
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />3
+            </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-adidas-duramo-lite-20-masculino/02/NQQ-0375-002/NQQ-0375-002_zoom2.jpg?ts=1577985531&ims=326x"
+          alt="Tenis" />
+        <strong>Tenis legal</strong>
+        <span>R$ 129,90</span>
+
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />3
+            </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-adidas-duramo-lite-20-masculino/02/NQQ-0375-002/NQQ-0375-002_zoom2.jpg?ts=1577985531&ims=326x"
+          alt="Tenis" />
+        <strong>Tenis legal</strong>
+        <span>R$ 129,90</span>
+
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />3
+            </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-adidas-duramo-lite-20-masculino/02/NQQ-0375-002/NQQ-0375-002_zoom2.jpg?ts=1577985531&ims=326x"
+          alt="Tenis" />
+        <strong>Tenis legal</strong>
+        <span>R$ 129,90</span>
+
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />3
+            </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-adidas-duramo-lite-20-masculino/02/NQQ-0375-002/NQQ-0375-002_zoom2.jpg?ts=1577985531&ims=326x"
+          alt="Tenis" />
+        <strong>Tenis legal</strong>
+        <span>R$ 129,90</span>
+
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />3
+            </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+    </ProductList>
+  );
 }
 
