e76a7153a666617ed3018f0bb508e5f998d3bd3c
SalesMga <lucassalessa3@gmail.com>
2020-04-29 02:13:39 -0300

    Estilizando Header (routes, stles, tags)

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {BrowserRouter} from 'react-router-dom';
+
+import GlobalStyles from './styles/global';
+import Routes from './routes';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Header />
+    <Routes />
+    <GlobalStyles />
+    </BrowserRouter>
+  );
+}
+
+export default App;
