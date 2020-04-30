3dd6062d8e749c8c653489ef1e09c8490cbc6fc2
SalesMga <lucassalessa3@gmail.com>
2020-04-30 00:38:03 -0300

    Estilizacao do carrinho

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,63 @@
 import React from 'react';
+import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md'
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <h1>Carrinho</h1>
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>Produto</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img src="https://static.netshoes.com.br/produtos/tenis-adidas-duramo-lite-20-masculino/02/NQQ-0375-002/NQQ-0375-002_zoom2.jpg?ts=1577985531&ims=326x"
+                alt="Tenis" />
+            </td>
+            <td>
+              <strong>Tênis adidas Durano 3.0</strong>
+              <span>R$ 120,00</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$ 258,80</strong>
+            </td>
+            <td>
+              <buttom type="buttom">
+                <MdDelete size={20} color="#7159c1" />
+              </buttom>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar pedido</button>
+
+        <Total>
+          <span>Total</span>
+          <strong>R$ 1920,00</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
 
 
