efc31a37b93d1996ca97fe40b7d47b7eb968e436
SalesMga <lucassalessa3@gmail.com>
2020-05-02 15:32:16 -0300

    Reactotron + Redux (ajuda no processo de debug e muito mais)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import './config/ReactotronConfig';
 import { Provider } from 'react-redux';
 import GlobalStyles from './styles/global';
 import Routes from './routes';
