c8eac23ffcbde8620711c03353e3853e563f489c
SalesMga <lucassalessa3@gmail.com>
2020-05-06 15:17:01 -0300

    Estoque na alteração

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -7,7 +7,7 @@ export default function cart(state = [], action) {
         const {product} = action;
 
         draft.push(product);
-        
+
       });
     case '@cart/REMOVE':
       return produce(state, draft => {
@@ -16,11 +16,7 @@ export default function cart(state = [], action) {
           draft.splice(productIndex, 1);
         }
       });
-    case '@cart/UPDATE_AMOUNT': {
-      if (action.amount <= 0) {
-        return state;
-      }
-
+    case '@cart/UPDATE_AMOUNT_SUCCESS': {
       return produce(state, draft => {
         const productIndex = draft.findIndex(p => p.id == action.id);
 
--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,10 +1,10 @@
 import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 
-import {toast} from 'react-toastify';
+import { toast } from 'react-toastify';
 import api from '../../../services/api';
 import { formatPrice } from '../../../util/format';
 
-import { addToCartSuccess, updateAmount } from './action';
+import { addToCartSuccess, updateAmountSuccess } from './action';
 
 function* addToCart({ id }) {
   const productExists = yield select(
@@ -18,13 +18,13 @@ function* addToCart({ id }) {
 
   const amount = currentAmount + 1;
 
-  if(amount > stockAmount){
+  if (amount > stockAmount) {
     toast.error('Quantidade solicitada fora de estoque');
     return;
   }
 
   if (productExists) {
-    yield put(updateAmount(id, amount));
+    yield put(updateAmountSuccess(id, amount));
   } else {
     const response = yield call(api.get, `/products/${id}`);
 
@@ -37,6 +37,20 @@ function* addToCart({ id }) {
   }
 }
 
+function* updateAmount({ id, amount }) {
+  if (amount <= 0) { return ; }
+
+  const stock = yield call(api.get, `stock/${id}`);
+  const stockAmount = stock.data.amount;
+
+  if (amount > stockAmount) {
+    toast.error('Quantidade solicitada fora de estoque');
+    return;
+  } else {
+    yield put(updateAmountSuccess(id, amount));
+  }
+}
 export default all([
   takeLatest('@cart/ADD_REQUEST', addToCart),
+  takeLatest('@cart/UPDATE_AMOUNT_REQUEST', updateAmount),
 ]);
